7567baf24de573d90e1c7686f3d13a32c9bc7ba5
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2026-08-12 02:41:25 -0400

    Sigil batch #2
    For some reason the board shifts to the right with Catch of the Day

---
+++ scripts/fight/sigils/Catch of the Day.gd
@@ -0,0 +1,15 @@
+extends Sigil
+
+func fish_data() -> Ruleset.CardData:
+	var bad_fish := get_config("fish_card", "Bad Fish") as String
+	var more_fish := get_config("fish_card", "More Fish") as String
+	var good_fish := get_config("fish_card", "Good Fish") as String
+	return Global.get_card_by_name([bad_fish, bad_fish, more_fish, good_fish].pick_random() as String)
+
+
+func on_card_perished(card: Card) -> void:
+	
+	if card != attached_card:
+		return
+
+	create_and_add_token(fish_data(), controller_id(), attached_card.id)
---
+++ scripts/fight/sigils/Double Strike.gd
@@ -0,0 +1,11 @@
+extends Sigil
+
+
+func on_card_attacked(card: Card) -> Array[CardAttackAction.StrikeGroup]:
+	if card != attached_card:
+		return []
+	var pos := oppose_pos(get_pos(card.id))
+	return [
+		CardAttackAction.StrikeGroup.new(pos).add_strike(card.id),
+		CardAttackAction.StrikeGroup.new(pos).add_strike(card.id),
+	]
