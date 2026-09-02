f4f795ad2063b4635a7a51cb16a5603bbcd51da1
??? ??? <whiterobot210@gmail.com>
2026-09-02 19:13:04 -0400

    slightly optimized hefty

--- scripts/fight/sigils/Hefty.gd
+++ scripts/fight/sigils/Hefty.gd
@@ -6,9 +6,9 @@ func trymove(card_pos: Vector2i, offset: int, row: Array[BoardManager.Slot]) -> 
 		move_card(row[readpos].card.id, Vector2i(readpos + offset, card_pos.y)) #move the card. The first card should always be the hefty boi, so no need to worry about it being null.
 		readpos += offset
 		if row[readpos].card == null: #if we find an empty space, break the chain.
-			break
+			return true
 
-	return (readpos < 0 || readpos >= row.size()) #if we got outside of the array, the shove-chain has hit a wall
+	return false #if we got outside of the array, the shove-chain has hit a wall
 
 
 
