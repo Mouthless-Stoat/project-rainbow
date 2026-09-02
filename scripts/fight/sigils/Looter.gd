fc14f3c6e4b62f3c87de63dfbb24bacda167ab64
??? ??? <whiterobot210@gmail.com>
2026-09-02 15:36:07 -0400

    fixed looter/side hustle
    looter and side hustle now properly draw cards equal to the card's power
    
    added config options to limit the number of cards drawn by looter/side hustle

--- scripts/fight/sigils/Looter.gd
+++ scripts/fight/sigils/Looter.gd
@@ -1,7 +1,9 @@
 extends Sigil
 
+func get_max() -> Ruleset.CardData:
+	return Global.get_card_by_name(get_config("looter_max_cards", 99) as String)
 
 func on_card_strike(striker: Card, pos: Vector2i, to_face: bool) -> void:
 	if striker != attached_card || !to_face:
 		return
-	draw_cards(DrawCardAction.Deck.MAIN, 1)
+	draw_cards(DrawCardAction.Deck.MAIN, min(striker.attack, get_max()) as int)
--- scripts/fight/sigils/Side Hustle.gd
+++ scripts/fight/sigils/Side Hustle.gd
@@ -1,7 +1,9 @@
 extends Sigil
 
+func get_max() -> Ruleset.CardData:
+	return Global.get_card_by_name(get_config("side_hustle_max_cards", 99) as String)
 
 func on_card_strike(striker: Card, pos: Vector2i, to_face: bool) -> void:
 	if striker != attached_card || !to_face:
 		return
-	draw_cards(DrawCardAction.Deck.SIDE, 1)
+	draw_cards(DrawCardAction.Deck.SIDE, min(striker.attack, get_max()) as int)
