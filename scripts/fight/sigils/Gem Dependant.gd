b06ba23be9a7c4382934f25a754d49401b621687
??? ??? <whiterobot210@gmail.com>
2026-09-04 00:13:12 -0400

    syntax change

--- scripts/fight/sigils/Gem Dependant.gd
+++ scripts/fight/sigils/Gem Dependant.gd
@@ -3,5 +3,5 @@ extends Sigil
 
 func on_any_action_resolved(act: Action) -> void:
 	var moxes : Card.Costs.Mox = fight_manager.get_moxes()
-	if moxes.green <= 0 && moxes.orange <= 0 && moxes.blue <= 0 :
+	if moxes.green <= 0 and moxes.orange <= 0 and moxes.blue <= 0 :
 		kill_card(attached_card.id)
